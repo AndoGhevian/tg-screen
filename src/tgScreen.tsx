d03c4c4d4fa56c6a6633cad57bc51ae15eaaf530
AndoGhevian <lenyvonbeer@gmail.com>
2024-11-18 15:37:12 +0400

    fix and improvie tgScreen implementation
    add ts, js docs
    fix netlify demo app

--- src/tgScreen.tsx
+++ src/tgScreen.tsx
@@ -14,6 +14,33 @@ import {
   miniApp,
 } from '@telegram-apps/sdk-react';
 
+const defaultMainButtonProps: MainButtonState = {
+  hasShineEffect: false,
+  isEnabled: true,
+  isLoaderVisible: false,
+  isVisible: false,
+  text: 'Main Button',
+  backgroundColor: '' as any,
+  textColor: '' as any,
+}
+
+const defaultSecondaryButtonProps: SecondaryButtonState = {
+  hasShineEffect: false,
+  isEnabled: true,
+  isLoaderVisible: false,
+  isVisible: false,
+  text: 'Secondary Button',
+  position: 'left',
+  backgroundColor: '' as any,
+  textColor: '' as any,
+}
+
+/**
+ * Before Use you need to manually mount appropriate components from
+ * telegram-apps sdk-react
+ * 
+ * https://docs.telegram-mini-apps.com/packages/telegram-apps-sdk/2-x/usage-tips#mount-used-components
+ */
 export function TgScreen({
   children,
   mainButtonProps,
@@ -26,6 +53,7 @@ export function TgScreen({
   backgroundColor,
   headerColor,
   bottomBarColor,
+  silentErrors = true,
 }: PropsWithChildren<{
   /**
    * https://docs.telegram-mini-apps.com/platform/methods#web-app-setup-main-button
@@ -41,8 +69,20 @@ export function TgScreen({
   } & Omit<Partial<SecondaryButtonState>, "isVisible">
   onBack?: () => void;
   onSettings?: () => void;
+  /**
+   * Defines if the confirmation dialog should be shown when user tries to close the screen
+   * @default true
+   */
   closeConfirmation?: boolean;
+  /**
+   * Defines if the screen should be expanded by default
+   * @default true
+   */
   autoExpand?: boolean;
+  /**
+   * If true, allows to expand or close the screen by swiping vertically
+   * @default true
+   */
   allowSwipeExpand?: boolean;
   /**
    * Can be eather RGB color or allowed theme color_key:
@@ -58,8 +98,21 @@ export function TgScreen({
    * Can be eather RGB color or allowed theme color_key:
    * https://docs.telegram-mini-apps.com/platform/methods#web-app-set-bottom-bar-color
    */
-  bottomBarColor?: string
+  bottomBarColor?: string;
+  /**
+   * if false original post evet errors will be thrown
+   * 
+   * NOTE: if silent true, then overrides postEvent property from mini app sdk init() method:
+   * https://docs.telegram-mini-apps.com/packages/telegram-apps-sdk/2-x/initializing
+   * @default true
+   */
+  silentErrors?: boolean;
 }>) {
+  const backButtonOffHandlerRef = React.useRef(() => {})
+  const mainButtonOffHandlerRef = React.useRef(() => {})
+  const secondaryButtonOffHandlerRef = React.useRef(() => {})
+  const settingsButtonOffHandlerRef = React.useRef(() => {})
+  
   const mainButtonIsMounted = useSignal(mainButton.isMounted)
   const secondaryButtonIsMounted = useSignal(secondaryButton.isMounted)
   const backButtonIsMounted = useSignal(backButton.isMounted)
@@ -87,7 +140,7 @@ export function TgScreen({
   }, [closeConfirmation, closingBehaviorIsMounted])
 
   useEffect(() => {
-    if (!swipeBehaviorIsMounted || !swipeBehaviorIsSupported) {
+    if (!swipeBehaviorIsMounted || (!swipeBehaviorIsSupported && silentErrors)) {
       return
     }
 
@@ -96,7 +149,7 @@ export function TgScreen({
     } else {
       swipeBehavior.disableVertical()
     }
-  }, [allowSwipeExpand, swipeBehaviorIsMounted])
+  }, [allowSwipeExpand, swipeBehaviorIsMounted, silentErrors])
 
   useEffect(() => {
     if (!viewportIsMounted) {
@@ -109,16 +162,18 @@ export function TgScreen({
   }, [autoExpand, viewportIsMounted])
 
   useEffect(() => {
-    if (!backButtonIsMounted || !backButtonIsSupported) {
+    if (!backButtonIsMounted || (!backButtonIsSupported && silentErrors)) {
       return
     }
 
     if (onBack) {
       backButton.show();
-      return backButton.onClick(onBack);
+      const offHandler = backButton.onClick(onBack);
+      backButtonOffHandlerRef.current = offHandler
+      return offHandler
     }
     backButton.hide();
-  }, [onBack, backButtonIsMounted]);
+  }, [onBack, backButtonIsMounted, silentErrors]);
 
   useEffect(() => {
     if (!mainButtonIsMounted) {
@@ -128,10 +183,13 @@ export function TgScreen({
     if (mainButtonProps) {
       const { onClick, ...restProps } = mainButtonProps
       mainButton.setParams({
+        ...defaultMainButtonProps,
         ...restProps,
         isVisible: true,
       })
-      return mainButton.onClick(onClick);
+      const offHandler = mainButton.onClick(onClick);
+      mainButtonOffHandlerRef.current = offHandler
+      return offHandler
     }
     if (mainButton.isVisible()) {
       mainButton.setParams({
@@ -148,10 +206,13 @@ export function TgScreen({
     if (secondaryButtonProps) {
       const { onClick, ...restProps } = secondaryButtonProps
       secondaryButton.setParams({
+        ...defaultSecondaryButtonProps,
         ...restProps,
         isVisible: true,
       })
-      return secondaryButton.onClick(onClick);
+      const offHandler = secondaryButton.onClick(onClick);
+      secondaryButtonOffHandlerRef.current = offHandler
+      return offHandler
     }
     if (secondaryButton.isVisible()) {
       secondaryButton.setParams({
@@ -161,46 +222,93 @@ export function TgScreen({
   }, [secondaryButtonProps, secondaryButtonIsMounted]);
 
   useEffect(() => {
-    if (!settingsButtonIsMounted || !settingsButtonIsSupported) {
+    if (!settingsButtonIsMounted || (!settingsButtonIsSupported && silentErrors)) {
       return
     }
 
     if (onSettings) {
       settingsButton.show();
-      return settingsButton.onClick(onSettings);
+      const offHandler = settingsButton.onClick(onSettings);
+      settingsButtonOffHandlerRef.current = offHandler
+      return offHandler
     }
     settingsButton.hide();
-  }, [onSettings, settingsButtonIsMounted]);
+  }, [onSettings, settingsButtonIsMounted, silentErrors]);
 
   useEffect(() => {
-    if(!miniAppIsMounted || !miniAppIsSupported) {
+    if(!miniAppIsMounted || (!miniAppIsSupported && silentErrors)) {
       return
     }
 
-    if(backgroundColor) {
-      miniApp.setBackgroundColor(backgroundColor as any)
-    }
-  }, [backgroundColor])
+    miniApp.setBackgroundColor((backgroundColor || 'bg_color') as any)
+  }, [miniAppIsMounted, backgroundColor, silentErrors])
 
   useEffect(() => {
-    if(!miniAppIsMounted || !miniAppIsSupported) {
+    if(!miniAppIsMounted || (!miniAppIsSupported && silentErrors)) {
       return
     }
 
-    if(headerColor) {
-      miniApp.setHeaderColor(headerColor as any)
-    }
-  }, [headerColor])
+    miniApp.setHeaderColor((headerColor || 'bg_color') as any)
+  }, [miniAppIsMounted, headerColor, silentErrors])
 
   useEffect(() => {
-    if(!miniAppIsMounted || !miniAppIsSupported) {
+    if(!miniAppIsMounted || (!miniAppIsSupported && silentErrors)) {
       return
     }
 
-    if(bottomBarColor) {
-      miniApp.setBottomBarColor(bottomBarColor as any)
+    miniApp.setBottomBarColor((bottomBarColor || 'bottom_bar_bg_color') as any)
+  }, [miniAppIsMounted, bottomBarColor, silentErrors])
+
+  useEffect(() => {
+    return () => {
+      backButtonOffHandlerRef.current()
+      mainButtonOffHandlerRef.current()
+      secondaryButtonOffHandlerRef.current()
+      settingsButtonOffHandlerRef.current()
+      
+      if(backButton.isMounted() && backButton.isSupported()) {
+        backButton.hide()
+      }
+
+      if(mainButton.isMounted() && mainButton.isVisible()) {
+        mainButton.setParams({
+          ...defaultMainButtonProps,
+          isVisible: false
+        })
+      }
+
+      if(secondaryButton.isMounted() && secondaryButton.isVisible()) {
+        secondaryButton.setParams({
+          ...defaultSecondaryButtonProps,
+          isVisible: false,
+        })
+      }
+
+      if(settingsButton.isMounted() && settingsButton.isSupported()) {
+        settingsButton.hide()
+      }
+
+      if(closingBehavior.isMounted()) {
+        closingBehavior.disableConfirmation()
+      }
+
+      if(swipeBehavior.isMounted() && swipeBehavior.isSupported()) {
+        swipeBehavior.disableVertical()
+      }
+
+      if(miniApp.isMounted() && miniApp.isSupported()) {
+        if(backgroundColor) {
+          miniApp.setBackgroundColor('bg_color')
+        }
+        if(headerColor) {
+          miniApp.setHeaderColor('bg_color')
+        }
+        if(bottomBarColor) {
+          miniApp.setBottomBarColor('bottom_bar_bg_color')
+        }
+      }
     }
-  }, [bottomBarColor])
+  }, [])
 
   return <>{children}</>;
 }
